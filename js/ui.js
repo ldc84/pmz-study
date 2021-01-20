d775dd2f3ae554ae5daca3df378de2b1dbdb9dcf
ldc84 <goodldc84@gmail.com>
2021-01-20 22:36:28 +0900

    faq 추가

--- js/ui.js
+++ js/ui.js
@@ -3,12 +3,23 @@ $(function(){
   var body = $('body');
   var menu = $('header .menu');
   var menuWrap = $('#menuWrap');
+  var dim = $('.dim');
   
   menu.on('click', function(){
     menu.toggleClass('active');
     menuWrap.toggleClass('active');
     body.toggleClass('active');
     return false;
+  });
+
+  dim.on('click', function(){
+
+    // menu.trigger('click');
+
+    menu.removeClass('active');
+    menuWrap.removeClass('active');
+    body.removeClass('active');
+
   });
   
   /*
@@ -19,5 +30,22 @@ $(function(){
     return false;
   });
   */
+
+  var faq = $('.faq-list');
+  var faqBtn = faq.find('.q');
+
+  faqBtn.on('click', function(){
+    var $this = $(this);
+    var parent = $this.parent('li');
+    var sibl = parent.siblings('li');
+
+    parent.toggleClass('active');
+    sibl.removeClass('active');
+
+    // $(this).parent('li').toggleClass('active');
+    // $(this).parent('li').siblings('li').removeClass('active');
+
+    return false;
+  });
 
 });
